64175beccbc577015d7caa4664cf7b7108e3e164
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-04-07 15:09:03 +0530

    organised files and added Card feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import ExpenseItem from './Components/ExpenseItem';
+//import React from 'react';
 
-function App() {
+import Expenses from './Components/Expenses/Expenses';
+const App=()=>{
   const expenses=[
   {
     title:"Zomato",
@@ -28,35 +29,23 @@ function App() {
   }
 
 ]
+//old approach importing react
+
+/* return React.createElement(
+  'div',
+  {},
+  React.createElement('h2',{},"Let's get started!"),
+  React.createElement(Expenses,{items:expenses})
+  ) */
+
+  //new modern jsx react one 
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-       title={expenses[0].title}
-       amount={expenses[0].amount}
-       location={expenses[0].location}
-       date={expenses[0].date}
-       />
-       <ExpenseItem
-       title={expenses[1].title}
-       amount={expenses[1].amount}
-       location={expenses[1].location}
-       date={expenses[1].date}
-       />
-       <ExpenseItem
-       title={expenses[2].title}
-       amount={expenses[2].amount}
-       location={expenses[2].location}
-       date={expenses[2].date}
-       />
-       <ExpenseItem
-       title={expenses[3].title}
-       amount={expenses[3].amount}
-       location={expenses[3].location}
-       date={expenses[3].date}
-      />
+      <Expenses items={expenses}/>
     </div>
   );
-}
+ }
 
 export default App;
---
+++ src/Components/Expenses/ExpenseDetails.js
@@ -0,0 +1,14 @@
+import './ExpenseItem.css';
+const ExpenseDetails=(props)=>{
+  return (
+    <div>
+        <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">{props.location}</div>
+            <div className="expense-item__price">₹{props.amount}</div>
+        </div>
+  </div>
+    );
+}
+
+export default ExpenseDetails;
