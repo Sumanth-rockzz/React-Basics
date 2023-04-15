095da577d42c5f26a53b5882b609f069d0e34f72
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-04-15 16:45:34 +0530

    added delete button checking for dynamic

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -5,10 +5,18 @@ import "./ExpenseItem.css";
 
 
 const ExpenseItem=(props)=>{
+  const tiltleclickHandler=()=>{
+    console.log("clicked");
+  }
+  const deleteclickHandler=()=>{
+    console.log("deleted");
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <ExpenseDetails amount={props.amount}  location={props.location}  title={props.title} />
+      <button onClick={tiltleclickHandler}>Change Title</button>
+      <button onClick={deleteclickHandler}>Delete</button>
     </Card>
   );
 }
